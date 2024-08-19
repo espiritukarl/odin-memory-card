c7fb26f3b6f42865a42001fd186051979979b261
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-08-19 14:16:07 +0800

    Created best score metric per difficulty

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,23 @@
-export default function Pokemon({ currentScore, bestScore }) {
+export default function Pokemon({
+  currentScore,
+  bestScore,
+  gameStart,
+  difficulty,
+}) {
+  function showBestScoreDifficulty() {
+    if (difficulty === "easy")
+      return <div className="score">Best Score (Easy): {bestScore.easy}</div>;
+    else if (difficulty === "medium")
+      return (
+        <div className="score">Best Score (Medium): {bestScore.medium}</div>
+      );
+    else if (difficulty === "hard")
+      return <div className="score">Best Score (Hard): {bestScore.hard}</div>;
+  }
   return (
-    <section className="score-container">
-      <div className="score">Current Score: {currentScore}</div>
-      <div className="score">Best Score: {bestScore}</div>
+    <section className={gameStart ? "score-container" : "game-start"}>
+      {gameStart && <div className="score">Current Score: {currentScore}</div>}
+      {showBestScoreDifficulty()}
     </section>
   );
 }
