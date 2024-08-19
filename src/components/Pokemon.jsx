08931851e7ce2b10629d60e813bd569ac5a5ac68
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-08-19 14:15:21 +0800

    Added classes for styling purposes

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,8 +1,10 @@
 export default function Pokemon({ poke, resetPokemonList }) {
   return (
-    <article onClick={() => resetPokemonList(poke.id)}>
-      <img src={poke.sprites.front_default} alt={poke.name} />
-      <h4>{poke.name}</h4>
+    <article className="pokemon">
+      <div className="pokemon-card" onClick={() => resetPokemonList(poke.id)}>
+        <img src={poke.sprites.front_default} alt={poke.name} />
+        <h4>{poke.name}</h4>
+      </div>
     </article>
   );
 }
