a861f82f8c951bc745febb764b18d6aa618b9e7d
Karl Vincent M. Espiritu <karlespiritukarl@gmail.com>
2024-08-20 14:27:08 +0800

    Added extra on hover effect for changing Pokemon image

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 export default function Pokemon({ poke, resetPokemonList }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
   return (
     <article className="pokemon">
-      <div className="pokemon-card" onClick={() => resetPokemonList(poke.id)}>
-        <img src={poke.sprites.front_default} alt={poke.name} />
+      <div
+        className="pokemon-card"
+        onClick={() => resetPokemonList(poke.id)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="image-container">
+          <img
+            src={poke.sprites.front_default}
+            alt={poke.name}
+            className={`pokemon-img ${isHovered ? "fade-out" : "fade-in"}`}
+          />
+          <img
+            src={poke.sprites.other["showdown"].front_default}
+            alt={poke.name}
+            className={`pokemon-img ${isHovered ? "fade-in" : "fade-out"}`}
+          />
+        </div>
         <h4>{poke.name}</h4>
       </div>
     </article>
